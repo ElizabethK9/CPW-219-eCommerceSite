be1c28af7361f3ade6b72a98c4cbfae00028218e
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2024-08-28 14:33:03 -0700

    commit

---
+++ CPW 219-eCommerceSite/Models/Merch.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW_219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single merch item avalible for purchase
+    /// </summary>
+    public class Merch
+    {
+        [Key]
+       
+        public int MerchId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+        //Todo: Add authentications
+    }
+}
