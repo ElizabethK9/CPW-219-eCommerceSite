fd7226cb6ec7a32277933c6e486e67c0c9c2ac4d
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2024-08-28 15:20:55 -0700

    Started the Create portion of CRUD

---
+++ CPW 219-eCommerceSite/Controllers/MerchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW_219_eCommerceSite.Controllers
+{
+    public class MerchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
