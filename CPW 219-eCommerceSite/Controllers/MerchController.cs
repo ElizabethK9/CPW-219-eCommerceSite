b2293d9060622d22da164410cf661694eaaa2f38
ElizabethK9 <167283322+ElizabethK9@users.noreply.github.com>
2024-08-28 15:38:34 -0700

    Create Game fully functional

--- CPW 219-eCommerceSite/Controllers/MerchController.cs
+++ CPW 219-eCommerceSite/Controllers/MerchController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW_219_eCommerceSite.Data;
+using CPW_219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW_219_eCommerceSite.Controllers
 {
     public class MerchController : Controller
     {
+        private readonly MerchKnicknacksContext _context;
+
+        public MerchController(MerchKnicknacksContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Merch merch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Merchendise.Add(merch);
+                _context.SaveChanges();
+
+                ViewData["Message"] = $"{merch.Title} was added successfully!";
+                //Show succes message on page
+                return View();
+            }
+
+            return View(merch);
         }
     }
 }
